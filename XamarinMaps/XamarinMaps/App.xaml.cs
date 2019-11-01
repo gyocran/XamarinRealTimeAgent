5a8c284532bfd28d5f183f4954e1c18df54e5eb1
George Yoku Ocran <gyocran@live.com>
2019-11-01 18:44:52 +0000

    Add project files.

---
+++ XamarinMaps/XamarinMaps/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinMaps
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            StartUp();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        private async void StartUp()
+        {
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+            if (status != PermissionStatus.Granted)
+            {
+                if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                {
+                    // await DisplayAlert("Need location", "App needs your location", "OK");
+                }
+
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+                //Best practice to always check that the key exists
+                if (results.ContainsKey(Permission.Location))
+                    status = results[Permission.Location];
+            }
+        }
+    }
+}
---
+++ XamarinMaps/XamarinMaps/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XamarinMaps
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_coords_Clicked(object sender, EventArgs e)
+        {
+            //if (!double.TryParse(txt_latitude.Text, out double lat))
+            //    return;
+            //if (!double.TryParse(txt_longitude.Text, out double lng))
+            //    return;
+
+            //Map.OpenAsync(lat, lng, new MapLaunchOptions
+            //{
+            //    NavigationMode = NavigationMode.None
+            //});
+        }
+
+        private void btnOpenMap_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MapPage());
+        }
+    }
+}
